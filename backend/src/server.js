b2ad2993b46228642c84f107a995799b2652bfc6
Le Trong Tri <trib2206019@student.ctu.edu.vn>
2025-12-20 11:24:48 +0700

    Implementing rate limiting with Arcjet

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { arcjectProtection } from "../middleware/arcject.middleware.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -17,6 +18,9 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
+
+// Rate limit
+app.use(arcjectProtection);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
